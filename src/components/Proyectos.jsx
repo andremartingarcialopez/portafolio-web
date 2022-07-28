54694dfa3a8d36e51321bf192719845c53f33386
Andy <andy_garlop4@hotmail.com>
2022-07-27 20:33:57 -0500

    actualizacion de proyectos

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -3,24 +3,35 @@ import React from 'react'
 const Proyectos = () => {
     return (
         <>
-            <div id='proyectos' className='text-center   h-auto lg:h-screen flex flex-col justify-center '>
+            <div id='proyectos' className='text-center   h-auto flex flex-col justify-center '>
                 <div data-aos="fade-up" data-aos-duration="1000">
                     <h2 className='background-titulos text-shadow titulos'>Mis Proyectos</h2>
                     <h3 className='subtitulos'>Me encanta poder ayudar a los demas con mis conocimientos y habilidades.</h3>
                 </div>
 
-                <div data-aos="fade-up" data-aos-duration="1000" className='flex flex-col justify-center items-center lg:flex-row gap-x-5 gap-y-8 '>
-                    <a href="https://garlo.netlify.app/" target="_blank">
-                        <img className='glass-object rounded-2xl w-[20rem]  lg:w-[23rem] xl:w-[25rem] hover:animate-pulse' width="100px" height="100px" src="img/garlo-web.webp" alt="imagen-garlo-web" />
-                    </a>
+                <div data-aos="fade-up" data-aos-duration="1000" className='flex flex-col justify-center items-center gap-x-5 gap-y-8 '>
 
-                    <a href="https://proy-cucei-mindbot.herokuapp.com/" target="_blank">
-                        <img className='glass-object rounded-2xl  w-[20rem] lg:w-[23rem] xl:w-[25rem] hover:animate-pulse' width="100px" height="100px" src="img/mindbot-web.webp" alt="imagen-mindbot-web" />
-                    </a>
+                    <div className='flex flex-col justify-center items-center'>
 
-                    <a href="https://happy-pet-citas.netlify.app/" target="_blank">
-                        <img className='glass-object rounded-2xl an w-[20rem] lg:w-[23rem] xl:w-[25rem] hover:animate-pulse' width="100px" height="100px" src="img/happypet-web.webp" alt="imagen-happyPet-web" />
-                    </a>
+                        <h4 className='font-bold text-[1.5rem] p-2 text-white/90'>Happy :) Pet</h4>
+                        <a href="https://happy-pet-citas.netlify.app/" target="_blank">
+                            <img className='glass-object rounded-2xl an w-[20rem] lg:w-[23rem] xl:w-[25rem] hover:animate-pulse' width="100px" height="100px" src="img/happypet-web.webp" alt="imagen-happyPet-web" />
+                        </a>
+                        <p className='w-[20rem] text-center pt-2 text-white/90 '>Simulacion de sitio web de agenda de mascotas, donde puedes agendar una cita como tambien poder editarla y eliminarla</p>
+                        <p className='w-[20rem] text-center pt-2 text-white/90 '> <span className='font-bold'>Tecnologias:</span> <span className='text-[#8677fc]'>Vite</span><span className='text-[#d6bb32]'>JS</span>, <span className='text-[#38bdf8]'>TailwindCSS</span></p>
+                    </div>
+
+                    <div className='flex flex-col justify-center items-center '>
+                        <h4 className='font-bold text-[1.5rem] p-2 text-white/90'>Shopy Car</h4>
+                        <a href="https://shopy-car.netlify.app/" target="_blank">
+                            <img className='glass-object rounded-2xl an w-[20rem] lg:w-[23rem] xl:w-[25rem] hover:animate-pulse' width="100px" height="100px" src="img/shopy-car.webp" alt="imagen-happyPet-web" />
+                        </a>
+                        <p className='w-[20rem] text-center pt-2 text-white/90 '>Simulacion de una tineda web de cursos en el cual podemos agregar y quitar cursos a nuestro carrito de compras</p>
+                        <p className='w-[20rem] text-center pt-2 text-white/90 '> <span className='font-bold'>Tecnologias:</span> <span className='text-[#E44D25]'>HTML</span>, <span className='text-[#264de4]'>CSS</span>, <span className='text-[#d6bb32]'>JavaScript</span></p>
+
+
+                    </div>
+
                 </div>
             </div>
         </>
